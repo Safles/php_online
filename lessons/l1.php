76471fa950eea848fa3f1cecb69115fc9dc760a1
Safles <kzandreicamilo10@gmail.com>
2024-11-22 22:54:38 +0800

    armari panget

---
+++ lessons/l1.php
@@ -0,0 +1,19 @@
+<button style="
+    height: 50px;
+    border: none;
+    border-radius: 5px;
+    background-color: #696969;
+">
+    <?php
+        // -single line comment
+        /*
+            Multi Line
+            Comment
+            Woah
+            Awesome
+        */
+        $name = "Kzandrei";
+        $age = 21;
+        echo("My name is " .$name. "and I am " .$age. " years young");
+    ?>
+</button>
